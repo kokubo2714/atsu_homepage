8ab01b6258bb3683b08b00e627ba66582403516e
Atsu <zm20302@josai.ac.jp>
2024-01-08 01:35:52 +0900

    test_5

--- tategase/author/tategase.js
+++ tategase/author/tategase.js
@@ -1,32 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     // すべてのトグルボタンにイベントリスナーを設定する
-    var toggleButtons = document.querySelectorAll('.toggle-button');
-    toggleButtons.forEach(function (button) {
-        // 元のテキストをdata属性に格納する
-        var originalText = button.textContent;
-        button.setAttribute('data-original-text', originalText);
+    let observe;
 
-        button.addEventListener('click', function () {
-            var content = this.nextElementSibling;
-            var isClosed = content.style.maxHeight && content.style.maxHeight !== '0px';
+    document.querySelectorAll('.toggle-button').forEach((button) => {
+        // 元のテキストをdata属性に格納する
+        button.addEventListener('click',  (event) => {
+            let content = button.nextElementSibling;
+            let isClosed = content.style.maxHeight && content.style.maxHeight !== '0px';
+
+            if (!observe) {
+                observe = new MutationObserver(() => {
+                    setTimeout(() => {
+                        content.style.maxHeight =  `${content.scrollHeight}px`;
+                    }, 500);
+                });
+            }
+
+            if (isClosed) {
+                observe.takeRecords();
+                observe.disconnect();
+            } else {
+                observe.observe(content, {attributes: true, attributeFilter: ["style"]});
+            }
 
             // 高さを切り替える
-            if (isClosed) {
-                content.style.maxHeight = null;
-                content.style.overflowY = 'hidden'; // スクロールバーを隠す
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-                content.style.overflowY = 'auto'; // スクロールバーを表示する
-            }
+            content.style.maxHeight = isClosed ? null : `${content.scrollHeight}px`;
 
             // ボタンのテキストを切り替える
-            if (isClosed) {
-                // 元のテキストを設定
-                this.textContent = this.getAttribute('data-original-text');
-            } else {
-                // 閉じるテキストを設定
-                this.textContent = '▼ ' + this.getAttribute('data-toggle-text') + ' を閉じる';
-            }
+            button.textContent = isClosed ? `▼ ${button.getAttribute('data-toggle-text')} を開く`: `▼ ${button.getAttribute('data-toggle-text')} を閉じる`;
         });
     });
+
 });
